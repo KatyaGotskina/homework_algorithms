9d096b8993b4d2e413f9fd24c64d093aff424ad1
KatyaGotskina <katya.gotskina@gmail.com>
2022-11-10 14:22:48 +0300

    Задача 2 из 2 дз добавлена 10 нояб 14:22

--- 2_hw/2.py
+++ 2_hw/2.py
@@ -13,4 +13,4 @@ def getMaximumGenerated(n: int) -> int:
             nums[2 * i] = nums[i]
         if 2 <= 2 * i + 1 <= n:
             nums[2 * i + 1] = nums[i] + nums[i + 1]
-    return nums    # Возвращает максимальный элемент в списке
+    return max(nums)    # Возвращает максимальный элемент в списке
