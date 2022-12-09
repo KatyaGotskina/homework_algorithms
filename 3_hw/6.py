7b17188acfd3e2cfc2cdd1f1bf79f7739be9a2be
KatyaGotskina <katya.gotskina@gmail.com>
2022-12-09 14:15:24 +0300

    Задача 6 добавлена 9 декабря

--- 3_hw/6.py
+++ 3_hw/6.py
@@ -1,28 +1,28 @@
+# Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None) -> None:
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 
-def maxDepth(root):
-    stack = [(root, 1, '')]
-    r_max_depth = 0
-    l_max_depth = 0
-
-    while stack:
-        cur_node, num, side = stack.pop()
-
-        if cur_node.left == None and cur_node.right == None:
-            if side == 'r':               
-                r_max_depth = max(r_max_depth, num)
-            if side == 'l':
-                l_max_depth = max(l_max_depth, num)
-            continue
-
-        if cur_node.left:
-            stack.append(cur_node.left, num + 1, 'l')
-        
-        if cur_node.right:
-            stack.append(cur_node.right, num + 1, 'r')
-    return r_max_depth + l_max_depth - 2
+def diameterOfBinaryTree(root):
+    if not root: 
+        return 0
+    d = {None: -1}
+    q = [root]
+    ans = 0
+    while q:
+        node = q[-1]
+        if node.left in d and node.right in d:
+            q.pop()
+            l = d[node.left] + 1
+            r = d[node.right] + 1
+            ans = max(ans, l + r)
+            d[node] = max(l, r)
+        else:
+            if node.left: 
+                q.append(node.left)
+            if node.right: 
+                q.append(node.right)
+    return ans
